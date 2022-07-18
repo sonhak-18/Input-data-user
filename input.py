cfea9f10482b7d29d2d32c0e0367b52b172121ca
sonhak-18 <fawaid1808@gmail.com>
2022-07-18 10:00:48 +0700

    first commit

---
+++ input.py
@@ -0,0 +1,12 @@
+#input user
+
+#data masih string
+data = input ("Isikan data: ")
+
+print("data = ",data,"type = ",type(data))
+
+#jika mengubah ke integer
+umur = float(input("isikan umur: "))
+umur = int(input("isikan umur: "))
+
+print("data = ",umur,",type =",type(umur))
